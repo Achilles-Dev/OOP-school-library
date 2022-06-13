4e1c724436f8f3b58a8c97431d32f73a7239be01
Solomon Achilles <solohag92@gmail.com>
2022-06-13 10:41:29 +0000

    Add Person class

---
+++ person.rb
@@ -0,0 +1,23 @@
+class Person
+  attr_reader :id
+  attr_accessor :name :age
+
+  def initialize(age, name = "Unknown", parent_permission = true)
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
+  end
+
+  def can_use_services?
+    true if @age >= 18 || @parent_permission == true
+  end
+
+  private
+
+  def is_of_age?
+    if (@age >= 18)
+      true
+    else false
+    end
+  end
+end
