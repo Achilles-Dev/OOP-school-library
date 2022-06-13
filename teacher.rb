d73c772001e091d00852f8391f462af145eadfae
Solomon Achilles <solohag92@gmail.com>
2022-06-13 11:13:22 +0000

    Add Teacher class

---
+++ teacher.rb
@@ -0,0 +1,12 @@
+require "./person.rb"
+
+class Teacher < Person
+  def initialize(age, specialization)
+    super(age)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
