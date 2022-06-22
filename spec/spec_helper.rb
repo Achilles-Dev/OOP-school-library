4ea978d1258c7a2b424d54b4eb2f292d3010dc2c
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-22 18:00:44 +0500

    add student spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 require_relative '../book'
 require_relative '../person'
+require_relative '../student'
 
 require 'json'
---
+++ spec/student_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe Student do
+  before :each do
+    @student = Student.new(21, "Grade 5", "Tobin", "true")
+  end
+
+  context 'Create @student, an instance of class Student' do
+    it 'takes one parameters and return a Student object' do
+      expect(@student).to be_instance_of Student
+    end
+  end
+
+  context 'Display age of default @student' do
+    it "should display '21' as @students's age" do
+      expected_value = 21
+      expect(@student.age).to eq(expected_value)
+    end
+  end
+end
