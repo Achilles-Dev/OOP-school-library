68c10bc8eebf56267c360cc8b11fc465f7255846
Benmuiruri <ben.muiruri13@gmail.com>
2022-06-22 17:22:49 +0300

    Add test to check whether student can use services

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -17,4 +17,13 @@ describe Student do
       expect(@student.age).to eq(expected_value)
     end
   end
+  
+  context 'Check whether student is of age to use service' do
+    it 'should return true if student can use service' do
+      @student.age = 13
+      @student.parent_permission = nil
+      can_use_services = @student.can_use_services?
+      expect(can_use_services).to be_falsey
+    end
+  end
 end
